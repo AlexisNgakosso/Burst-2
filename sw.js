dd47d8ef83729d3c115715e7198d5c2d37065488
AlexisNgakosso <jeremyngakosso.nj@gmail.com>
2026-06-13 17:24:52 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+// Burst - Service Worker
+// Permet à l'app de fonctionner hors ligne
+
+const CACHE_NAME = 'burst-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Installation : mise en cache des fichiers essentiels
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activation : suppression des anciens caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Fetch : servir depuis le cache, sinon réseau
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cached => cached || fetch(event.request))
+      .catch(() => caches.match('./index.html'))
+  );
+});
